ae179da4e5023eed1667e2d988f3cfff19bf3749
Seb <lecampus@users.noreply.github.com>
2024-01-08 10:50:08 +0100

    exo-2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,28 @@
 // you can write js here
 console.log('exo-2');
+
+var _isTesting_ = true;
+
+const weekend = "WEEK-END"
+const week = "SEMAINE =/"
+
+let myDate = new Date;
+let day = myDate.getDay()
+let hour = myDate.getHours()
+
+if(_isTesting_) {
+	day = prompt("Day")
+	hour = prompt("Hour")
+}
+
+console.log({day, hour})
+if(day == 0 || day == 6) {
+	console.log(weekend)
+} else {
+	if(day == 1 && hour < 9) 		// lundi < 9h
+		console.log(weekend)
+	else if(day == 5 && hour > 17)	// Vendredi > 17h
+		console.log(weekend)
+	else
+		console.log(week)
+}
