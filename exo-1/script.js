46e88f38d3a6e55c0c4d329056d03f06d151896d
Seb <lecampus@users.noreply.github.com>
2024-01-08 10:10:03 +0100

    exo-1

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,10 @@
 // you can write js here
 console.log('hello from file');
+
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+const celsius = kelvins - 273;
+const fahrenheits = Math.floor(celsius * (9/5) + 32);
+
+console.log(kelvins+"°K")
+console.log(celsius+"°C")
+console.log(fahrenheits+"°F")
